c65323fa98538f7b389161ae5df71e0c58918f4f
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 16:16:23 -0400

    Add BID3 to file

--- challenge3.rb
+++ challenge3.rb
@@ -7,4 +7,9 @@
 
 
 # Break It Down:
+# First we have to create an array. That array has to have strings.
+# Look at each string... (.each) (||)
+# Use method that will only give us strings that begin with t. (maybe ==)
+# End goal is to have code give us the strings that begin with t.
+
 
