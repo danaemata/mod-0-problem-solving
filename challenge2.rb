340aa7e59d65760a4c19b1707ffbefcb39057e76
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 14:39:31 -0400

    Add simple directions in file

--- challenge2.rb
+++ challenge2.rb
@@ -5,3 +5,11 @@
 
 # Create an array with strings. Those strings have to be mixed with capitalized and lowercase letters. The code will print out all of the strings in lowercase.
 
+
+# Break It Down:
+
+# First we have to create an array with strings. Make sure all strings(words) are both capitalized and lowercase. AMOUNT IS UP TO ME.
+# Have to look at each string... (.each method) (do) (||)
+# Use a method that will print every character in lowercase letters. (.downcase method) 
+# End goal is to have code print out all words in lowercase!!!!
+
