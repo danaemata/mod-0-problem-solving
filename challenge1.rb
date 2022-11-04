6c14d8a2e7b69ceb8a20b83615530d50d26e4752
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 14:24:18 -0400

    Add clarifying steps and code to file

--- challenge1.rb
+++ challenge1.rb
@@ -5,3 +5,18 @@
 # First create an array that has strings. The output must give us ONLY the strings that have 4 characters.
 
 
+# Break It Down:
+
+# First, you have to create an array with strings. CAN BE HOW EVER MANY YOU WANT.
+# Have to look at each string... (.each method)
+# Check if word length is exactly 4 characters (if) (.length method) (== 4)
+# End goal is to have the code print ONLY the strings that have 4 characters.
+
+
+groceries = ["milk", "bread", "water", "eggs", "meat", "chips"]
+
+groceries.each do |ingredients|
+    if ingredients.length == 4
+        p ingredients
+    end
+end
