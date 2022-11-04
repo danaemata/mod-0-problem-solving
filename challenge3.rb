32a48d3e95932e0ffcb9a8af661c66d11b9bfdd8
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 15:37:57 -0400

    Add overall goal3 to file

--- challenge3.rb
+++ challenge3.rb
@@ -1,1 +1,10 @@
 #Given an array of strings, return only the words that begin with the letter "t".
+
+
+# Overall Goal:
+
+# Create an array with strings. The code has to print out words that begin with the letter t.
+
+
+# Break It Down:
+
