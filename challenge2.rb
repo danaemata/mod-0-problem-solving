ac638d4f826005cb2b14179f3ff355e9ca833370
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 14:34:09 -0400

    Add overall goal2 to file

--- challenge2.rb
+++ challenge2.rb
@@ -1,1 +1,7 @@
 # Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+
+# Overall Goal:
+
+# Create an array with strings. Those strings have to be mixed with capitalized and lowercase letters. The code will print out all of the strings in lowercase.
+
