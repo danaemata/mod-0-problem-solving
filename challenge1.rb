fb014fe81bec225f5a463066edbeee7ba01a4478
Danae Mata <danae.mata1515@gmail.com>
2022-11-04 14:12:46 -0400

    Add goal to file

--- challenge1.rb
+++ challenge1.rb
@@ -1,1 +1,7 @@
 #Given an array of strings, return only the strings that have exactly 4 characters.
+
+# Overall Goal:
+
+# First create an array that has strings. The output must give us ONLY the strings that have 4 characters.
+
+
